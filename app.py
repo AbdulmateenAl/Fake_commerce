54fe9830d908cb86ea4d4c3475166437253f8c2f
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-03-17 16:12:55 +0100

    added a json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.route('/<name>', methods=['GET'])
 def home(name=None):
     return render_template('index.html', person=name)
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    with open('static/data/fakeproducts.json', "r", encoding="utf-8") as f:
+        products = json.load(f)
+    print(products)
+    #return redirect('https://fakestoreapi.com/products')
+    return jsonify(products)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
